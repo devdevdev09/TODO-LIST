9fffae3b06719c3c076ee2233cb8a4594c28b8b0
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-03-20 23:49:41 +0900

    add db repository

--- src/main/java/com/heo/todo/Repository/DbMainRepository.java
+++ src/main/java/com/heo/todo/Repository/DbMainRepository.java
@@ -9,33 +9,26 @@ import com.heo.todo.entity.MainEntity;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MemoryMainRepository implements MainRepository{
+public class DbMainRepository implements MainRepository{
     private static Map<Long, MainEntity> list = new HashMap<>();
     private static Long sequence = 0L;
     
     @Override
     public MainEntity save(MainEntity main) {
-        main.setId(++sequence);
-        list.put(main.getId(), main);
         
-        return main;
+        return null;
     }    
 
     @Override
     public MainEntity replace(MainEntity main) {
-        Long seq = main.getId();
-        list.replace(seq, main);
 
-        return main;
+        return null;
     }    
 
-    // @todo...
     @Override
     public MainEntity update(MainEntity main) {
-        main.setId(++sequence);
-        list.put(main.getId(), main);
         
-        return main;
+        return null;
     }    
 
     @Override
