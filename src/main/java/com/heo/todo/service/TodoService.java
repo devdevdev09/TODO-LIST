faf4e54a7f24bc4c151f6e7e0dd15d511e8ff7af
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-03-22 22:08:14 +0900

    add todoservice

---
+++ src/main/java/com/heo/todo/service/TodoService.java
@@ -0,0 +1,14 @@
+package com.heo.todo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface TodoService {
+    // 1. 초기 등록
+    // 2. 중간 업데이트
+    // 3. 완료 처리
+    // 4. 기타 상태 처리(지연 업데이트 등)
+    // 5. 상태 조회
+
+    // etc. 회원 관리, 알림 관리
+}
