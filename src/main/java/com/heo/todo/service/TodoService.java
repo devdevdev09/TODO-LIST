a7c97d5560318dcb8aa347abc7c226a80dcecacd
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-03-25 23:08:00 +0900

    update service date

--- src/main/java/com/heo/todo/service/TodoService.java
+++ src/main/java/com/heo/todo/service/TodoService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface TodoService {
+    // ~ 03.28
     // 1. 초기 등록
         // 등록방법, 단순 한개씩, 여러개 동시, 업로드 등등?
         // 기존거 복사 기능?
@@ -12,11 +13,13 @@ public interface TodoService {
     // 4. 기타 상태 처리(지연 업데이트 등)
     // 5. 상태 조회
 
+    // ~ 04.04
     // 알림관리
     // 1. 지연알림
     // 2. 지속알림(remind)
     // 3. 완성알림X
 
+    // ~ 04.11
     // 아주 뒤로..........일단 개인용
     // 회원관리
     // 1. OAuth 2.0
