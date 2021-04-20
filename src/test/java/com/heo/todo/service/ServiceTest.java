1b481abf187a1f5d0fb3f652dfcd3d94e1dc0739
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-04-20 22:58:59 +0900

    add service test

---
+++ src/test/java/com/heo/todo/service/ServiceTest.java
@@ -0,0 +1,13 @@
+package com.heo.todo.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ServiceTest {
+    
+    @Test
+    public void sampleTest(){
+        assertNotEquals("true", "false");
+    }
+}
