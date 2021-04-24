11ab60bda3d5569b08913fd068c243295aa54f41
devdevdev09 <devdevdevdevdev09@gmail.com>
2021-04-24 17:59:24 +0900

    update history entity

--- src/main/java/com/heo/todo/entity/History.java
+++ src/main/java/com/heo/todo/entity/History.java
@@ -1,11 +1,31 @@
 package com.heo.todo.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.heo.todo.enums.Status;
+import com.heo.todo.enums.Title;
+import com.heo.todo.enums.Type;
 
 import lombok.Data;
 
 @Entity
 @Data
 public class History {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     
+    Long todoid; // ref
+
+    @Column(columnDefinition = "TIMESTAMP")
+    LocalDateTime regdt;
+
+    Status status;
+    Title title;
+    Type type;
 }
